cff2b549170e61afdee6d4977da5f0530a2f2c54
tantairs <1161308098@qq.com>
2016-04-03 05:17:35 +0800

    添加动态规划的两种实现方法

--- data_structure_with_java/CutRod.java
+++ data_structure_with_java/CutRod.java
@@ -5,9 +5,14 @@ package com.lianxi.data_structure_with_java;
  */
 public class CutRod {
 
+    int[] r = null;
+
     public static void main(String[] args){
         int[] array = {1,5,8,9,10,17,17,20,24,30};
-        int result = doCutRod(array,10);
+//        int result = doCutRod(array,10);
+        CutRod cutRod = new CutRod();
+        int result = cutRod.doCutRodMemoized(array,0);
+
         System.out.println(result);
     }
     //利用递归的方法解决钢条分割的问题,注意这里的数组下标与n的取值范围.
@@ -21,5 +26,40 @@ public class CutRod {
                 q = temp;
         }
         return q;
+    }
+
+    //使用动态规划的自顶向下的过程来求分割
+    public  int doCutRodMemoized(int[] price,int n){
+
+        r = new int[n+1];
+        for(int i = 0; i <= n; i++){
+            r[i] = -10000;
+        }
+        return memoizedCutRodAux(price,n,r);
+    }
+
+    public  int memoizedCutRodAux(int[] price,int n,int[] r){
+        int q = -1;
+        if(r[n] >= 0)
+            return r[n];
+        if(n == 0)
+            q = 0;
+        else {
+            q = -10000;
+        }
+        for(int i = 1; i <= n; i++){
+            int temp = price[i-1] + memoizedCutRodAux(price,n-i,r);
+            if(q < temp)
+                q = temp;
+        }
+        r[n] = q;
+        return q;
+    }
+
+
+    //使用动态规划的自低向上的过程来求解
+    public static int doBottomUpCutRod(int[] price,int n){
+
+        return 0;
     }
 }
