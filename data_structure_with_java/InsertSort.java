c2408d57530d612c24469e2a2ed72a614fe81478
tantairs <1161308098@qq.com>
2016-04-04 03:18:41 +0800

    算法学习

--- data_structure_with_java/InsertSort.java
+++ data_structure_with_java/InsertSort.java
@@ -4,4 +4,25 @@ package com.lianxi.data_structure_with_java;
  * Created by tantairs on 16/4/4.
  */
 public class InsertSort {
+
+    public static void main(String[] args){
+
+        int[] array = {5,2,4,6,1,3};
+        doInsertSort(array);
+        ArrayUtils.printArray(array);
+    }
+
+    public static void doInsertSort(int[] array){
+        int key = 0;
+        int j = 0;
+        for(int i = 1; i < array.length; i++){
+            key = array[i];
+            j = i - 1;
+            while (j >= 0 && array[j] > key){
+                array[j+1] = array[j];
+                j = j-1;
+            }
+            array[j+1] = key;
+        }
+    }
 }
--- data_structure_with_java/MergeSort.java
+++ data_structure_with_java/MergeSort.java
@@ -4,4 +4,44 @@ package com.lianxi.data_structure_with_java;
  * Created by tantairs on 16/4/4.
  */
 public class MergeSort {
+
+    public static void main(String[] args){
+        int[] array = {2,4,5,7,1,2,3,6};
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.doMergeSort(array,0,array.length-1);
+        ArrayUtils.printArray(array);
+    }
+
+    public void doMergeSort(int[] array,int p,int r){
+        if(p < r){
+            int q = (p+r)/2;
+            doMergeSort(array,p,q);
+            doMergeSort(array,q+1,r);
+            merge(array,p,q,r);
+        }
+    }
+
+    public void merge(int[] array,int p,int q,int r){
+        int n1 = q-p+1;
+        int n2 = r-q;
+        int[] left = new int[n1+1];
+        int[] right = new int[n2+1];
+        for(int i = 0; i < n1; i++)
+            left[i] = array[p+i];
+        for(int j = 0; j < n2; j++)
+            right[j] = array[q+1+j];
+        left[n1] = Integer.MAX_VALUE;
+        right[n2] = Integer.MAX_VALUE;
+        int i = 0;
+        int j = 0;
+        for(int k = p; k <= r; k++){
+            if(left[i] <= right[j]){
+                array[k] = left[i];
+                i = i+1;
+            }else {
+                array[k] = right[j];
+                j = j+1;
+            }
+        }
+    }
 }
