5c8d5d49c8b59e42795bf0bca4fc156a6870249d
tantairs <1161308098@qq.com>
2016-04-18 22:43:31 +0800

    算法 一书相应的练习

--- data_structure_with_java/Print1_1_5.java
+++ data_structure_with_java/Print1_1_5.java
@@ -1,7 +1,28 @@
 package com.lianxi.data_structure_with_java;
 
+import java.util.Scanner;
+
 /**
  * Created by tantairs on 16/4/15.
  */
 public class Print1_1_5 {
+    public static void main(String[] args){
+
+        double a = 0.0, b = 0.0;
+        System.out.println("输入两个double类型的数");
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        boolean result = judgeNumber(a,b);
+        System.out.println(result);
+
+    }
+
+    public static boolean judgeNumber(double x, double y){
+        if((x <= 1 && x >=0)&&(y <= 1 && y >= 0) )
+            return true;
+        else {
+            return false;
+        }
+    }
 }
--- data_structure_with_java/PrintArray1_1_11.java
+++ data_structure_with_java/PrintArray1_1_11.java
@@ -1,7 +1,45 @@
 package com.lianxi.data_structure_with_java;
 
+import java.util.Random;
+
 /**
  * Created by tantairs on 16/4/15.
  */
 public class PrintArray1_1_11 {
+    public static void main(String[] args){
+        boolean[][] a = new boolean[10][10];
+        a = initRandom(a);
+        printArray(a);
+
+
+    }
+
+    public static void printArray(boolean[][] a){
+        for(int i = 0; i < 10; i++){
+            System.out.print(" " + i);
+            System.out.println(" ");
+            for(int j = 0; j < 10; j++){
+                if(a[i][j])
+                    System.out.print("*" + " ");
+                else
+                    System.out.print(" " + " ");
+                System.out.println(" ");
+            }
+        }
+    }
+
+    public static boolean[][] initRandom(boolean[][] a){
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                Random random = new Random(System.currentTimeMillis());
+                int result = random.nextInt(10);
+                if(result >= 5)
+                    a[i][j]= true;
+                else {
+                    a[i][j]= false;
+                }
+            }
+        }
+        return a;
+    }
 }
